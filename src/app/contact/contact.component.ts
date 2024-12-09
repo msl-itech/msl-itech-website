08799f2f0bc8a89f13b1fd241d363e6684666a18
JuniorMSL <junior@msl-itech.com>
2024-12-09 22:23:19 +0100

    iframe

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,22 +7,47 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  isLoading: boolean = true; // Indicateur de chargement
-  constructor(private spinner: NgxSpinnerService) {}
-  ngOnInit(): void {
-    this.spinner.show();
-    this.adjustHeight();  
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 1000);
-  }
+  // isLoading: boolean = true; // Indicateur de chargement
+  // constructor(private spinner: NgxSpinnerService) {}
+  // @ViewChild('odooIframe', { static: false }) iframe!: ElementRef; // Référence à l'iframe
+  // ngOnInit(): void {
+  //   this.spinner.show();
+  //   this.adjustHeight();  
+  //   setTimeout(() => {
+  //     /** spinner ends after 5 seconds */
+  //     this.spinner.hide();
+  //   }, 1000);
+
+  //   window.addEventListener('message', (event) => {
+  //   // On vérifie que c'est bien notre message attendu
+  //   if (event.data && event.data.type === 'IFRAME_BOTTOM_REACHED') {
+  //     // Ici, l'iframe est arrivé en bas.
+  //     // Vous pouvez par exemple forcer un léger défilement du parent
+  //     // ou mettre un indicateur visuel pour montrer que le scrolling parent est disponible.
+      
+  //     // Par exemple, on peut tenter un petit scroll du parent pour "sortir" du focus iframe.
+  //     window.scrollBy(0, 1);
+  //   }
+  // });
+  // }
  
-  @HostListener('window:resize')
-  adjustHeight() {
-    const iframeContainer = document.getElementById('iframeContainer');
-    if (iframeContainer) {
-      iframeContainer.style.height = `${window.innerHeight}px`;
-    }
-  }
+  // @HostListener('window:resize')
+  // adjustHeight() {
+  //   const iframeContainer = document.getElementById('iframeContainer');
+  //   if (iframeContainer) {
+  //     iframeContainer.style.height = `${window.innerHeight}px`;
+  //   }
+  // }
+  // @ViewChild('iframe') iframe: ElementRef<HTMLIFrameElement> | undefined;
+
+  // ngOnInit() {
+  //   // Écouter les messages provenant de l'iframe
+  //   window.addEventListener('message', (event) => {
+  //     if (event.data?.type === 'IFRAME_HEIGHT' && this.iframe) {
+  //       const iframeElement = this.iframe.nativeElement;
+  //       // Ajuster la hauteur de l'iframe selon la hauteur reçue
+  //       iframeElement.style.height = `${event.data.height}px`;
+  //     }
+  //   });
+  // }
 }
