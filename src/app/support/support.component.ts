7fd3f2d4e8db9b1065dea312014172701dd2db73
JuniorMSL <junior@msl-itech.com>
2025-03-01 13:57:23 +0100

    support page

--- src/app/support/support.component.ts
+++ src/app/support/support.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
-  styleUrl: './support.component.css'
+  styleUrl: './support.component.css',
 })
 export class SupportComponent {
-
+  scrollToFaq(event: Event): void {
+    event.preventDefault();
+    const faqSection = document.getElementById('faq');
+    if (faqSection) {
+      faqSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
 }
